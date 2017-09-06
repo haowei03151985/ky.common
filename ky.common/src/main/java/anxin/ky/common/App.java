e52b3d78883141afb0532566b94c80fbdf14c38a
haowei03151985 <zhuweirong2008@163.com>
2017-09-06 17:01:50 +0800

    初始化项目

---
+++ ky.common/src/main/java/anxin/ky/common/App.java
@@ -0,0 +1,13 @@
+package anxin.ky.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
